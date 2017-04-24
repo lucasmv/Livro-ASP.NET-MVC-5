e6d489c29ae81f298c65d7a0537457d2af8539aa
lucas silva <lucas.mv.silva@gmail.com>
2017-04-24 06:56:22 -0300

    Capitulo 3 Inicial

---
+++ Capitulo_3/Projeto1/Projeto1/Controllers/CategoriasController.cs
@@ -0,0 +1,101 @@
+﻿using Projeto1.Contexts;
+using Projeto1.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Projeto01.Controllers
+{
+    public class CategoriasController : Controller
+    {
+
+        private EFContext context = new EFContext();
+
+        public ActionResult Index()
+        {
+            return View(context.Categorias.OrderBy(x => x.Nome));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            context.Categorias.Add(categoria);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var categoria = context.Categorias.Find(id);
+
+            if (categoria == null)
+                return HttpNotFound();
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(categoria).State = EntityState.Modified;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        public ActionResult Details(long id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var categoria = context.Categorias.Find(id);
+
+            if (categoria == null)
+                return HttpNotFound();
+
+            return View(categoria);
+        }
+
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var categoria = context.Categorias.Find(id);
+
+            if (categoria == null)
+                return HttpNotFound();
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            var categoria = context.Categorias.Find(id);
+            context.Categorias.Remove(categoria);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
