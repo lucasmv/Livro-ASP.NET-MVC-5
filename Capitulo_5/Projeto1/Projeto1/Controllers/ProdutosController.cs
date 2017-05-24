c9a86cd2fd8d0e86a95701fa5dbd4d5cffd283a3
Casa <Casa@DESKTOP-9TDPC3M>
2017-05-23 21:17:14 -0300

    Final capitulo 05

---
+++ Capitulo_5/Projeto1/Projeto1/Controllers/ProdutosController.cs
@@ -0,0 +1,124 @@
+﻿using Modelo.Cadastros;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Servico.Tabelas;
+using Servico.Cadastros;
+
+namespace Projeto1.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private ProdutoServico produtoServico = new ProdutoServico();
+        private CategoriaServico categoriaServico = new CategoriaServico();
+        private FabricanteServico fabricanteServico = new FabricanteServico();
+
+        private ActionResult ObterVisaoProdutoPorId(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = produtoServico.ObterProdutoPorId((long)id);
+
+            if (produto == null)
+                return HttpNotFound();
+
+            return View(produto);
+        }
+
+        private void PopularViewBag(Produto produto = null)
+        {
+            if (produto == null)
+            {
+                ViewBag.CategoriaId = new SelectList(categoriaServico.ObterCategoriasClassificadasPorNome(), "CategoriaId", "Nome");
+                ViewBag.FabricanteId = new SelectList(fabricanteServico.ObterFabricantesClassificadosPorNome(), "FabricanteId", "Nome");
+            }
+            else
+            {
+                ViewBag.CategoriaId = new SelectList(categoriaServico.ObterCategoriasClassificadasPorNome(), "CategoriaId", "Nome", produto.CategoriaId);
+                ViewBag.FabricanteId = new SelectList(fabricanteServico.ObterFabricantesClassificadosPorNome(), "FabricanteId", "Nome", produto.FabricanteId);
+            }
+        }
+
+        private ActionResult GravarProduto(Produto produto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    produtoServico.GravarProduto(produto);
+                    return RedirectToAction("Index");
+                }
+                return View(produto);
+            }
+            catch
+            {
+                return View(produto);
+            }
+        }
+
+
+        public ActionResult Index()
+        {
+            return View(produtoServico.ObterProdutosClassificadosPorNome());
+        }
+
+        public ActionResult Details(long? id)
+        {
+            return ObterVisaoProdutoPorId(id);
+        }
+
+        public ActionResult Create()
+        {
+            PopularViewBag();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Produto produto)
+        {
+            return GravarProduto(produto);
+        }
+
+        public ActionResult Edit(long? id)
+        {
+            PopularViewBag(produtoServico.ObterProdutoPorId((long)id));
+            return ObterVisaoProdutoPorId(id);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Produto produto)
+        {
+            return GravarProduto(produto);
+        }
+
+        public ActionResult Delete(long? id)
+        {
+            return ObterVisaoProdutoPorId(id);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            try
+            {
+                Produto produto = produtoServico.EliminarProdutoPorId(id);
+
+                TempData["Message"] = "Produto " + produto.Nome.ToUpper() + " foi removido";
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
