35e8ffd75c65fb34c66cbadcb303ade03e37ae42
Casa <Casa@DESKTOP-9TDPC3M>
2017-05-25 20:34:02 -0300

    Inicio Capitulo 7

---
+++ Capitulo_7/Projeto1/Projeto1/Areas/Seguranca/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using Projeto1.Infraestrutura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto1.Areas.Seguranca.Controllers
+{
+    public class AdminController : Controller
+    {
+        private GerenciadorUsuario GerenciadorUsuario
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<GerenciadorUsuario>();
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View(GerenciadorUsuario.Users);
+        }
+    }
+}
